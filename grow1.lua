b86996bafea2a509066f2b613251f12b3d8329cd
Hayden <Haydenmarkins1@gmail.com>
2025-05-19 05:29:43 -0500

    Update and rename grow.lua to grow1.lua

--- grow1.lua
+++ grow1.lua
@@ -1,17 +1,15 @@
 local button = script.Parent
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
+local player = game:GetService("Players").LocalPlayer
 local backpack = player:WaitForChild("Backpack")
 
 button.MouseButton1Click:Connect(function()
-	local character = player.Character
-	if not character then return end
-
+	local character = player.Character or player.CharacterAdded:Wait()
 	local tool = character:FindFirstChildOfClass("Tool")
+
 	if tool then
 		local dupe = tool:Clone()
 		dupe.Parent = backpack
-		print("[XOTIC] Duplicated:", dupe.Name)
+		print("[XOTIC] Duped tool:", dupe.Name)
 	else
 		warn("No tool found to duplicate.")
 	end
